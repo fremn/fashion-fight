ec35ec9b6acf55a2cc185998f94586645fdba714
Fremen DeRuvo <fremenderuvo@gmail.com>
2013-10-17 14:13:45 -0500

    make bb store data in rails

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ FashionFight::Application.routes.draw do
 
   resources :contests, :only => [:index, :show]
   root 'contests#index'
+
+
+  scope :api do
+   post 'contests/:id/comments' => 'contests#comment', :as => :contest_comment_api
+  end
 end
